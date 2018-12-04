c52395444e3e218a523d0ff75a79bd8a7c87bbf7
navspeak <navneet.sahay@gmail.com>
2018-12-04 10:41:08 +0530

    NearestSmallestElement

---
+++ src/stacks/InfixToReversePolishNotation.java
@@ -0,0 +1,11 @@
+package stacks;
+/*
+Precedence:
+^ | * / ~ | + - | ( | =
+
+a + b * (c-d) => abcd-*+
+f=a*c^k/p-q*g^(h-b)  => fack^*p/qghb-^*-=
+
+ */
+public class InfixToReversePolishNotation {
+}
