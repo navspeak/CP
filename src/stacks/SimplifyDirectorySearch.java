eb550c45c7b3e2ea0c7b509a185ef9deb1a3c1c6
navspeak <navneet.sahay@gmail.com>
2018-12-16 20:06:37 +0530

    Permutation

--- src/stacks/SimplifyDirectorySearch.java
+++ src/stacks/SimplifyDirectorySearch.java
@@ -7,4 +7,33 @@ path = "/a/./b/../../c/", => "/c"
 Note that absolute path always begin with ‘/’ ( root directory )
 Path will not have whitespace characters.
  */
+package stacks;
+public class SimplifyDirectorySearch {
 
+    public String simplifyPath(String A) {
+    return null;
+    }
+
+}
+
+
+class SimplifyDirSearchTest{
+    public static void main(String[] args) {
+        SimplifyDirectorySearch test = new SimplifyDirectorySearch();
+        System.out.println(
+                test.simplifyPath("/home").equals("/home") ?
+                        "Test 1 Passed" : "Test 1 Failed");
+        System.out.println(
+                test.simplifyPath("/home/").equals("/home") ?
+                        "Test 2 Passed" : "Test 2 Failed");
+        System.out.println(
+                test.simplifyPath("/home/..").equals("/") ?
+                        "Test 3 Passed" : "Test 3 Failed");
+        System.out.println(
+                test.simplifyPath("/home/../a/./b").equals("/a/b") ?
+                        "Test 4 Passed" : "Test 4 Failed");
+        System.out.println(
+                test.simplifyPath("/a/./b/../../c/").equals("/c") ?
+                        "Test 5 Passed" : "Test 5 Failed");
+    }
+}
